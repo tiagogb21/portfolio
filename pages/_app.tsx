a1e564d2ca5151eb5ba22e67116bce7a9d2f7e0b
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-06-21 11:48:19 -0300

    criando estilo para o componente app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,9 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="grid grid-cols-12 gap-6">
-      <div>Sidebar</div>
-      <div>
+    <div className="grid grid-cols-12 gap-6 px-48 my-14">
+      <div className="col-span-3 bg-white rounded-2xl">Sidebar</div>
+      <div className="col-span-9 bg-white">
         <Component {...pageProps} />
       </div>
     </div>
